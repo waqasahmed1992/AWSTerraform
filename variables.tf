16e666669d466e802f7ca2a0bf8f706674cf784a
waqasahmed1992 <forwardtowaqas@gmail.com>
2022-08-29 17:22:40 +0500

    adding git ignore

--- variables.tf
+++ variables.tf
@@ -1,5 +1,11 @@
-variable aws_region {
+variable "aws_region" {
   type        = string
   default     = "us-east-1"
+  description = "description"
+}
+
+variable "env" {
+  type        = string
+  default     = "Dev"
   description = "description"
 }
